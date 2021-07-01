361483397afa07fbe095be0be0a7d0660fe3586a
William Young <wadyoung@outlook.com>
2021-07-01 14:12:21 +0100

    Added more tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,8 +2,9 @@ require "docking_station"
 require "bike"
 
 describe DockingStation do
-  let(:bike) {subject.release_bike}
+  let(:bike) {DockingStation.new.release_bike}
   
-  it {is_expected.to respond_to :release_bike}
-  it {expect(bike).to be_working}
+  it { is_expected.to respond_to :release_bike }
+  it { expect(bike).to be_working }
+  it { is_expected.to respond_to(:dock).with(1).argument }
 end
