9ee51e3af6b9853e009c154e1d399193133af4dc
Pav Rao <pav0107@gmail.com>
2021-07-02 16:34:45 +0100

    step 18 dealing with broken bikes, user 1

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,9 @@ require "bike"
 
 describe Bike do
   it {is_expected.to respond_to :working?}
+
+  it 'can be reported as broken' do
+    subject.report_broken
+    expect(subject.broken?).to eq true
+  end
 end
