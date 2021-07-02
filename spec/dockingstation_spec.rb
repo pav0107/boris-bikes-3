3047b84ba6adac8c6dffe1c216cd2fd85950e472
Pav Rao <pav0107@gmail.com>
2021-07-02 10:13:20 +0100

    changed 'bike is working'

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,15 +1,20 @@
 require "docking_station"
 
-describe DockingStation do
-  # let(:bike) {DockingStation.new.release_bike}
-  
+describe DockingStation do  
   describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect {subject.release_bike}.to raise_error "No bikes available"
     end
   end
 
-  # it { expect(bike).to be_working }
+  it "bike is working" do
+    docking_station = DockingStation.new
+    bike1 = Bike.new
+    docking_station.dock(bike1)
+    bike = docking_station.release_bike
+    expect(bike.working?).to eq(true) 
+  end
+
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
   it "docks something" do
