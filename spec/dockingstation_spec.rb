b92e8c7168813aefd67cd7de0c90ed5b4a8914a4
Pav Rao <pav0107@gmail.com>
2021-07-02 12:17:00 +0100

    step 14 wrapping collections

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -9,7 +9,7 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock Bike.new }.to raise_error "Docking station full"
     end
   end
@@ -22,15 +22,15 @@ describe DockingStation do
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
   it "docks something" do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq subject.bikes
   end
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes.pop).to eq bike
   end
 
 end
