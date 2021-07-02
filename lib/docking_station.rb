6a33c700f176ea828f0501b9864192ca226cdef5
Pav Rao <pav0107@gmail.com>
2021-07-02 13:09:06 +0100

    step 16 removing magic numbers

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,6 +3,8 @@ require_relative "bike"
 class DockingStation
 
   attr_reader :bikes
+
+  DEFAULT_CAPACITY = 20
   
   def initialize
     @bikes = []
@@ -21,7 +23,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= 20
+    @bikes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
